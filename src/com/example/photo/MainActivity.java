de9d7e9e3d1a7e12100d04c41980ccf4412db2e2
junun123 <ghodakega@hotmail.com>
2013-04-13 15:39:44 +0530

    second commit
    crop function added

--- src/com/example/photo/MainActivity.java
+++ src/com/example/photo/MainActivity.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	private static final int PIC_CROP = 2;
+	private Uri picUri;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +29,58 @@ public class MainActivity extends Activity {
 	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 	    startActivityForResult(takePictureIntent, actionCode);
 	}
+// this is app for crop operaion of image
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (resultCode == RESULT_OK) {
+	    
+	picUri = data.getData();
+	performCrop();
+	    }
+	    else if(requestCode == PIC_CROP){
+			//get the returned data
+			Bundle extras = data.getExtras();
+			//get the cropped bitmap
+			Bitmap thePic = extras.getParcelable("data");
+			
+			//retrieve a reference to the ImageView
+			ImageView picView = (ImageView)findViewById(R.id.picture);
+			//display the returned cropped image
+			picView.setImageBitmap(thePic);
+		}
+	}
+	private void performCrop(){
+		
+		final int PIC_CROP = 2;
+		try {
+		}
+		catch(ActivityNotFoundException anfe){
+		    //display an error message
+		    String errorMessage = "Whoops - your device doesn't support the crop action!";
+		    Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+		    toast.show();
+		}
+		//keep track of cropping intent
+		
+	    //call the standard crop action intent (the user device may not support it)
+		Intent cropIntent = new Intent("com.android.camera.action.CROP");
+		    //indicate image type and Uri
+		cropIntent.setDataAndType(picUri, "image/*");
+		    //set crop properties
+		cropIntent.putExtra("crop", "true");
+		    //indicate aspect of desired crop
+		cropIntent.putExtra("aspectX", 1);
+		cropIntent.putExtra("aspectY", 1);
+		    //indicate output X and Y
+		cropIntent.putExtra("outputX", 256);
+		cropIntent.putExtra("outputY", 256);
+		    //retrieve data on return
+		cropIntent.putExtra("return-data", true);
+		    //start the activity - we handle returning in onActivityResult
+		startActivityForResult(cropIntent, PIC_CROP);
+	
+		
+	}
+
+
 
 }// this is app for taking image
